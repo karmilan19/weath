5d69f01721de28ff1c2ce819f5ec23b91bc114b4
karthick <karmilan19@gmail.com>
2020-12-04 16:12:51 +0530

    time

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -35,8 +35,24 @@ export class DashboardComponent implements OnInit {
   location9Report;
   location9;
 
+  location1Err = 0;
+  location2Err = 0;
+  location3Err = 0;
+  location4Err = 0;
+  location5Err = 0;
+  location6Err = 0;
+  location7Err = 0;
+  location8Err = 0;
+  location9Err = 0;
 
-  constructor(private http: HttpClient) { }
+  mySubscription: Subscription;
+
+
+  constructor(private http: HttpClient) { 
+    this.mySubscription= interval(30000).subscribe((x =>{
+      this.doStuff();
+  }));
+  }
 
   ngOnInit() {
     // Simple GET request with response type <any>
@@ -51,8 +67,7 @@ export class DashboardComponent implements OnInit {
     console.log( location )
     
     this.http.get<any>('http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=f62ee3c16258619e3299c577cb68fb28&units=Metric').subscribe(data => {
-      console.log( data );
-
+      this.errorRep(type, 0)
       switch(type){
         case 1:
           this.location1Report = data;
@@ -84,8 +99,50 @@ export class DashboardComponent implements OnInit {
       }
 
       
+    }, (error) => {
+      // Here you can handle errors
+      console.log( 'error' )
+      this.errorRep(type, 1)
     });
     
   }
+
+  errorRep(ty, ct){
+    
+    switch(ty){
+      case 1:
+        this.location1Err = ct;
+        break;
+      case 2:
+        this.location2Err = ct;
+        break;
+      case 3:
+        this.location3Err = ct;
+        break;
+      case 4:
+        this.location4Err = ct;
+        break;
+      case 5:
+        this.location5Err = ct;
+        break;
+      case 6:
+        this.location6Err = ct;
+        break;
+      case 7:
+        this.location7Err = ct;
+        break;
+      case 8:
+        this.location8Err = ct;
+        break;
+      case 9:
+        this.location9Err = ct;
+        break;
+    }
+  }
+
+  doStuff(){
+    
+    console.log( '333' );
+}
 
 }
